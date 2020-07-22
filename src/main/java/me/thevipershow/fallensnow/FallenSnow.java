4a362f168fea643f66d2a3bf3f4f54402776421e
TheViperShow <thevipershowita@gmail.com>
2020-07-22 18:48:44 +0200

    Updated command.

--- src/main/java/me/thevipershow/fallensnow/FallenSnow.java
+++ src/main/java/me/thevipershow/fallensnow/FallenSnow.java
@@ -1,5 +1,6 @@
 package me.thevipershow.fallensnow;
 
+import me.thevipershow.fallensnow.command.FallenSnowCommand;
 import me.thevipershow.fallensnow.config.Values;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,5 +13,8 @@ public final class FallenSnow extends JavaPlugin {
         final Values values = Values.getInstance(this);
         final ParticleManager particleManager = ParticleManager.getInstance(this, values);
         Bukkit.getWorlds().forEach(w -> particleManager.getEnabledWorlds().put(w, true));
+        particleManager.startAnimation();
+        final FallenSnowCommand command = new FallenSnowCommand(this, values, particleManager);
+        Bukkit.getPluginCommand("fsnow").setExecutor(command);
     }
 }
